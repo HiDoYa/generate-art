88ce293dafdc3a7d4c995e52e6af5f0f80f0cca2
HiDoYa <hiroyagojo@gmail.com>
2019-07-24 21:06:27 -0700

    Update model hyperparameters

--- dcgan_model.py
+++ dcgan_model.py
@@ -15,26 +15,27 @@ class Models:
     @staticmethod
     def buildGenerator(vectSize, weightsPath=None):
         model = Sequential()
+        shape = (8, 8, 1024)
 
-        model.add(Dense(65536, input_dim=vectSize))
-        model.add(Reshape((8, 8, 1024)))
-        model.add(LeakyReLU())
+        model.add(Dense(shape[0] * shape[1] * shape[2], input_dim=vectSize))
+        model.add(Reshape(shape))
+        model.add(Activation("relu"))
 
+        model.add(Conv2DTranspose(1024, kernel_size=5, strides=2, padding="same"))
+        model.add(BatchNormalization())
+        model.add(Activation("relu"))
+        
         model.add(Conv2DTranspose(512, kernel_size=5, strides=2, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
-        
+        model.add(Activation("relu"))
+
         model.add(Conv2DTranspose(256, kernel_size=5, strides=2, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(Activation("relu"))
 
         model.add(Conv2DTranspose(128, kernel_size=5, strides=2, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
-
-        model.add(Conv2DTranspose(64, kernel_size=5, strides=2, padding="same"))
-        model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(Activation("relu"))
 
         model.add(Conv2DTranspose(3, kernel_size=5, strides=1, padding="same"))
         model.add(Activation("tanh"))
@@ -50,26 +51,27 @@ class Models:
     def buildDiscriminator(imgRows, imgCols, numChannels, weightsPath=None):
         model = Sequential()
         inputShape = (imgRows, imgCols, numChannels)
+        reluAlpha = 0.2
 
         model.add(Conv2D(64, kernel_size=5, strides=2, padding="same", input_shape=inputShape))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(LeakyReLU(alpha=reluAlpha))
 
         model.add(Conv2D(128, kernel_size=5, strides=2, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(LeakyReLU(alpha=reluAlpha))
 
-        model.add(Conv2D(256, kernel_size=5, strides=1, padding="same"))
+        model.add(Conv2D(256, kernel_size=5, strides=2, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(LeakyReLU(alpha=reluAlpha))
 
         model.add(Conv2D(512, kernel_size=5, strides=2, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(LeakyReLU(alpha=reluAlpha))
 
-        model.add(Conv2D(1024, kernel_size=5, strides=2, padding="same"))
+        model.add(Conv2D(1024, kernel_size=5, strides=1, padding="same"))
         model.add(BatchNormalization())
-        model.add(LeakyReLU())
+        model.add(LeakyReLU(alpha=reluAlpha))
 
         model.add(Flatten())
         model.add(Dense(1))
